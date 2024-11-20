5cec4270e4a3cb98db54574192371c040b7ec2b1
BartlomiejSadza <bartlomiej.sadza@icloud.com>
2024-11-20 20:03:59 +0100

    Hula na localhost ;p

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import express, { Request, Response } from 'express';
+import { fetchWeatherData } from './fetchApi.js';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Siema z backendu w TypeScript!');
+});
+
+app.get('/weather', async (req: Request, res: Response) => {
+    try {
+        const weatherData = await fetchWeatherData();
+        res.json(weatherData);
+    } catch (error) {
+        res.status(500).send('Error fetching weather data');
+    }
 });
 
 app.listen(PORT, () => {
